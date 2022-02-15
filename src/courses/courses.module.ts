442a43165d1a4cf6d68b24cfab6a2d9c451b5e05
devAndre-Isaac <andreisaac344@gmail.com>
2022-02-15 20:35:20 -0300

    feat: add providers

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CoursesService } from 'src/courses.service';
 import { CoursesController } from './courses.controller';
 
 @Module({
   controllers: [CoursesController],
+  providers: [CoursesService],
 })
 export class CoursesModule {}
