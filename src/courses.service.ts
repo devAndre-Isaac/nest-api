f1c65740e40d915abfc47fed7297becaa22691ec
devAndre-Isaac <andreisaac344@gmail.com>
2022-02-15 20:27:00 -0300

    feat: :sparkles: add CoursesService

--- src/courses.service.ts
+++ src/courses.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './courses/entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do Nest.JS',
+      description: 'Fundamentos do Nest.JS',
+      tags: ['node.js', 'nestjs', 'typescript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course: Course) => course.id === Number(id));
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const courseIndex = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+    this.courses[courseIndex] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const courseIndex = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+    if (courseIndex >= 0) {
+      this.courses.splice(courseIndex, 1);
+    }
+  }
+}
