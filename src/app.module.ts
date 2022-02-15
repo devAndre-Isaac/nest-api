280d9c9ccb8bfd6827aa9d4fca43af9940b94ec2
devAndre-Isaac <andreisaac344@gmail.com>
2022-02-15 20:10:16 -0300

    feat: :sparkles: add service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { CourseService } from './course/course.service';
 
 @Module({
   imports: [CoursesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CourseService],
 })
 export class AppModule {}
